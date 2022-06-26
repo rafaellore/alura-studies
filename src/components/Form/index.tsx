51e917f40061227d3c3bfbfa21eed380f1a0940d
rafaellore <birdneon3@gmail.com>
2022-06-26 15:44:06 -0300

    feat(List): adds list component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -6,13 +6,27 @@ export default class Form extends React.Component {
     return (
       <form>
         <div>
-          <label>Tarefa</label>
-          <input type="text" />
+          <label htmlFor="tarefa">Adicione um novo estudo</label>
+          <input
+            type="text"
+            name="tarefa"
+            id="tarefa"
+            placeholder="O que você quer estudar"
+            required
+          />
         </div>
 
         <div>
           <label>Tempo</label>
-          <input type="time" />
+          <input
+            type="time"
+            step="1"
+            name="tempo"
+            id="tempo"
+            min="00:00:00"
+            max="01:30:00"
+            required
+          />
         </div>
 
         <Button>Botão</Button>
---
+++ src/components/List/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function List() {
+  const tasks = [
+    {
+      task: 'React',
+      time: '02:00:00',
+    },
+    {
+      task: 'JavaScript',
+      time: '01:00:00',
+    },
+  ];
+
+  return (
+    <aside>
+      <h2>Estudos do dia</h2>
+
+      <ul>
+        {tasks.map((item, index) => (
+          <li key={index}>
+            <h3>{item.task}</h3>
+            <span>{item.time}</span>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
