94d9058675af233b0d3d9d853ba83c4545b56898
rafaellore <birdneon3@gmail.com>
2022-07-05 17:23:32 -0300

    feat: adds state on list component

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,24 +1,35 @@
+import { useState } from 'react';
 import Item from './Item';
 import style from './List.module.scss';
 
 export default function List() {
-  const tasks = [
+  const [list, setList] = useState([
     {
       task: 'React',
       time: '02:00:00',
     },
     {
-      task: 'JavaScript',
+      task: 'Javascript',
       time: '01:00:00',
     },
-  ];
+    {
+      task: 'Typescript',
+      time: '03:00:00',
+    },
+  ]);
 
   return (
     <aside className={style.tasks}>
-      <h2>Estudos do dia</h2>
+      <h2
+        onClick={() =>
+          setList([...list, { task: 'Estudar estado', time: '05:00:00' }])
+        }
+      >
+        Estudos do dia
+      </h2>
 
       <ul>
-        {tasks.map((item, index) => (
+        {list.map((item, index) => (
           <Item key={index} {...item} />
         ))}
       </ul>
