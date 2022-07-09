02a904d6e86ed5f3bc31b255c0bd2ac0e55b5434
rafaellore <birdneon3@gmail.com>
2022-07-09 15:49:04 -0300

    refactor(List): adds list with selected item

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import Chronometer from '../components/Chronometer';
 import Form from '../components/Form';
 import List from '../components/List';
+import { Itask } from '../types/task';
 import style from './App.module.scss';
 
 function App() {
+  const [tasks, setTasks] = useState<Itask[] | []>([]);
+  const [selected, setSelected] = useState<Itask>();
+
+  function selectTask(selectedTask: Itask) {
+    setSelected(selectedTask);
+  }
+
+  console.log(selected);
+
   return (
     <div className={style.AppStyle}>
-      <Form />
-      <List />
-      <Chronometer/>
+      <Form setTasks={setTasks} />
+      <List tasks={tasks} selectTask={selectTask} />
+      <Chronometer />
     </div>
   );
 }
---
+++ src/types/task.ts
@@ -0,0 +1,7 @@
+export interface Itask {
+  task: string;
+  time: string;
+  selected: boolean;
+  completed: boolean;
+  id: string;
+}
